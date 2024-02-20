03fab0f2ab8a3d65dc124d0493d62f36ea761315
sagnikcapital <sagnikd@capitalnumbers.com>
2024-02-20 13:12:26 +0530

    service

---
+++ app/Config/UserApi.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class UserApi extends BaseConfig {
+  
+}
--- app/Controllers/API/AuthController.php
+++ app/Controllers/API/AuthController.php
@@ -4,11 +4,39 @@ namespace App\Controllers\API;
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
+use App\Services\AuthService;
 
 class AuthController extends BaseController
 {
-    public function index()
+    protected $auth;
+
+    /**
+     * Initialize Auth
+     *
+     * @param AuthService $auth
+     */
+    public function __construct(AuthService $auth)
     {
-        //
+        $this->auth = $auth;
+    }
+
+    /**
+     * User or Customer Add
+     *
+     * @return void
+     */
+    public function userRegister()
+    {
+        return $this->response->setJSON(['message' => 'welcome', 'status' => 200]);
+    }
+
+    /**
+     * User or Customer login via OTP
+     *
+     * @return void
+     */
+    public function userLogin()
+    {
+        return $this->response->setJSON(['message' => 'logged in', 'status' => 200]);
     }
 }
