854d9f7ba5cbff9ae68100a012337cd7b0f50e84
sagnikcapital <sagnikd@capitalnumbers.com>
2024-02-21 16:46:07 +0530

    admin-seeder

---
+++ app/Controllers/API/BookingController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers\API;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class BookingController extends BaseController
+{
+    public function myTrips()
+    {
+        //
+    }
+}
