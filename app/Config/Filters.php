b7c7d40818fd533237434d7bb7da0a87e2534988
sagnikcapital <sagnikd@capitalnumbers.com>
2024-02-23 15:41:06 +0530

    cors-resolved

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -39,6 +39,7 @@ class Filters extends BaseConfig
             // 'honeypot',
             // 'csrf',
             // 'invalidchars',
+            'cors'
         ],
         'after' => [
             'toolbar',
