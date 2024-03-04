b90cac116fa3e1f7eeb3e345fec0991f324d3d42
sagnikcapital <sagnikd@capitalnumbers.com>
2024-03-04 16:56:50 +0530

    read-md

---
+++ app/Cells/APICell.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Cells;
+
+use CodeIgniter\View\Cells\Cell;
+use CodeIgniter\Model;
+
+class APICell extends Cell
+{
+    /**
+     * Customer, Booking, Transaction list table 
+     *
+     * @param Model $model
+     * @param array $condition
+     * @return void
+     */
+    public function displayTable(Model $model, array $condition)
+    {
+        
+    }
+}
---
+++ app/Cells/api.php
@@ -0,0 +1,200 @@
+<style>
+    body {
+	background-color: #f0f0f0;
+	font: 12px Arial, Helvetica, sans-serif;
+	color: #2e2e2e;
+	padding-top: 42px;
+}
+
+div.layout-978 { width: 978px; margin: 0px auto; }
+
+div.tblcontainer {
+	padding: 18px;
+	border: 1px solid #c0c0c0;
+	margin: 20px auto;
+	background: url('../images/splitter.png') repeat-x 0 206px #fff;
+	-moz-border-radius: 4px;
+	-webkit-border-radius: 4px;
+	border-radius: 4px; /* future proofing */
+	-khtml-border-radius: 4px; /* for old Konqueror browsers */
+	width: 93%;	
+}
+
+
+
+.datagrid {
+	border-collapse: collapse;
+}
+
+.datagrid thead tr th {
+	color: #949494;
+	font-size: 11px;
+	font-weight: normal;
+	text-align: left;
+	padding: 6px 8px;
+	border: 1px solid #c9c9c9;
+	background-image: linear-gradient(bottom, #EEEFF1 50%, #F9F9F9 54%);
+	background-image: -moz-linear-gradient(bottom, #EEEFF1 50%, #F9F9F9 54%);
+	background-image: -webkit-linear-gradient(bottom, #EEEFF1 50%, #F9F9F9 54%);
+	background-image: -ms-linear-gradient(bottom, #EEEFF1 50%, #F9F9F9 54%);
+	background-image: -webkit-gradient(linear, left bottom, left top, color-stop(0.5, #EEEFF1), color-stop(0.54, #F9F9F9)); 
+}
+.datagrid tbody tr {
+	background: #fff;
+}
+
+.datagrid tbody tr td {
+	padding: 6px 8px;
+	border: 1px solid #c9c9c9;
+}
+
+
+.datagrid tbody tr td.cash {
+	text-align: right;
+}
+</style>
+<div>
+    <!-- Your HTML here -->
+  <div class="layout-978">
+    <div class="tblcontainer">
+        <table class="datagrid">
+            <thead>
+                <tr>
+                    <th>FAID <img src="http://pixelblot.com/test/-mortgage/images/icon-arrow-up.png"/></th>
+                    <th>Loan/Ref # <img src="http://pixelblot.com/test/-mortgage/images/icon-arrow-up.png"/></th>
+                    <th>Borrower Name <img src="http://pixelblot.com/test/-mortgage/images/icon-arrow-up.png"/></th>
+                    <th>Purpose <img src="http://pixelblot.com/test/-mortgage/images/icon-arrow-up.png"/></th>
+                    <th>Loan Type <img src="http://pixelblot.com/test/-mortgage/images/icon-arrow-up.png"/></th>
+                    <th>Amount <img src="http://pixelblot.com/test/-mortgage/images/icon-arrow-up.png"/></th>
+                    <th>Rate <img src="http://pixelblot.com/test/-mortgage/images/icon-arrow-up.png"/></th>
+                    <th>Coordinator <img src="http://pixelblot.com/test/-mortgage/images/icon-arrow-up.png"/></th>
+                    <th>Estimated Closing Date <img src="http://pixelblot.com/test/-mortgage/images/icon-arrow-up.png"/></th>
+                    <th>Alerts <img src="http://pixelblot.com/test/-mortgage/images/icon-arrow-up.png"/></th>
+                    <th>Status <img src="http://pixelblot.com/test/-mortgage/images/icon-arrow-up.png"/></th>
+                    <th>&nbsp; </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>XXXX</td>
+                    <td>062120</td>
+                    <td>Sal Morrison</td>
+                    <td>Purchase</td>
+                    <td>15 yr. Conf Fixed </td>
+                    <td class="cash">$450,000</td>
+                    
+                    <td>3.20%</td>
+                    <td>Joan Smith</td>
+                    <td>02/12/14 </td>
+                    <td>3</td>
+                    <td>Prep for Fin. Underwrite - Bank, Client</td>
+                    <td><span class="icon-add" style="color:#556996;">+</span></td>
+                </tr>
+                <tr style="background-color:#556996; color:#fff; font-weight:bold;">
+                    <td>XXXX</td>
+                    <td>062120</td>
+                    <td>Jake Freeman</td>
+                    <td>Purchase</td>
+                    <td>15 yr. Conf Fixed </td>
+                    <td class="cash">$450,000</td>
+                    
+                    <td>3.21%</td>
+                    <td>Randy Jackson</td>
+                    <td>03/12/14 </td>
+                    <td>2</td>
+                    <td>Prep for Fin. Underwrite - Bank, Client</td>
+                    <td><span class="icon-add" style="color:#fff;">+</span></td>
+                </tr>
+                <tr>
+                    <td>XXXX</td>
+                    <td>062120</td>
+                    <td>Lori Schwartz</td>
+                    <td>Purchase</td>
+                    <td>15 yr. Conf Fixed </td>
+                    <td class="cash">$450,000</td>
+                    
+                    <td>2.12%</td>
+                    <td>Joan Smith</td>
+                    <td>01/10/14 </td>
+                    <td>0</td>
+                    <td>Prep for Fin. Underwrite - Bank, Client</td>
+                    <td><span class="icon-add" style="color:#556996;">+</span></td>
+                </tr>
+                <tr>
+                    <td>XXXX</td>
+                    <td>062120</td>
+                    <td>John Rascal</td>
+                    <td>Purchase</td>
+                    <td>15 yr. Conf Fixed </td>
+                    <td class="cash">$450,000</td>
+                    
+                    <td>1.50%</td>
+                    <td>Joan Smith</td>
+                    <td>01/12/14 </td>
+                    <td>1</td>
+                    <td>Application Registerd</td>
+                    <td><span class="icon-add" style="color:#556996;">+</span></td>
+                </tr>
+                <tr>
+                    <td>XXXX</td>
+                    <td>062120</td>
+                    <td>John Rascal</td>
+                    <td>Purchase</td>
+                    <td>15 yr. Conf Fixed </td>
+                    <td class="cash">$450,000</td>
+                    
+                    <td>1.50%</td>
+                    <td>Joan Smith</td>
+                    <td>01/12/14 </td>
+                    <td>1</td>
+                    <td>Application Registerd</td>
+                    <td><span class="icon-add" style="color:#556996;">+</span></td>
+                </tr>
+                <tr>
+                    <td>XXXX</td>
+                    <td>062120</td>
+                    <td>John Rascal</td>
+                    <td>Purchase</td>
+                    <td>15 yr. Conf Fixed </td>
+                    <td class="cash">$450,000</td>
+                    
+                    <td>1.50%</td>
+                    <td>Joan Smith</td>
+                    <td>01/12/14 </td>
+                    <td>1</td>
+                    <td>Application Registerd</td>
+                    <td><span class="icon-add" style="color:#556996;">+</span></td>
+                </tr>
+                <tr>
+                    <td>XXXX</td>
+                    <td>062120</td>
+                    <td>Rufida Sanchez</td>
+                    <td>Purchase</td>
+                    <td>15 yr. Conf Fixed </td>
+                    <td class="cash">$450,000</td>
+                    <td>2.50%</td>
+                    <td>Joan Smith</td>
+                    <td>12/12/14 </td>
+                    <td>0</td>
+                    <td>xxxxx</td>
+                    <td><span class="icon-add" style="color:#556996;">+</span></td>
+                </tr>
+                                    <tr>
+                    <td>XXXX</td>
+                    <td>062120</td>
+                    <td>Rufida Sanchez</td>
+                    <td>Purchase</td>
+                    <td>15 yr. Conf Fixed </td>
+                    <td class="cash">$450,000</td>
+                    <td>2.50%</td>
+                    <td>Joan Smith</td>
+                    <td>12/12/14 </td>
+                    <td>0</td>
+                    <td>xxxxx</td>
+                    <td><span class="icon-add" style="color:#556996;">+</span></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  </div>
+</div>
